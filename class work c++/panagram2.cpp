68bb3ca2ae0e7c4d45251385498fd345c893e52a
vaishnavi_kumbhar <vaishnavisatish9096@gmail.com>
2023-06-30 07:00:38 +0530

    commit

--- class work c++/panagram2.cpp
+++ class work c++/panagram2.cpp
@@ -1,1 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+string panagram(string str)
+{
+    vector<bool>present{26,false};
+    int index;
+    char c;
+    for(int i=0;i<str.length;i++)
+    {
 
+        c = char(tolower(str[i]));
+        if(c>='a'&&c<='z')
+        {
+            index = c-'a';
+        }
+        else
+        {
+            present[index]=true;
+        }
+    }
+    for(int i=0;i<=25;i++)
+    {
+        if(present[i]==false)
+        {
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<panagram(s);
+    return 0;
+}
